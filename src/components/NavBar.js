cf896f6cf51738ec0b5d5cd5324c55540aaee041
Daniel Morales <daniel.morales23@outlool.cl>
2023-12-14 22:47:30 -0300

    Add component

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = ()=>{
+    return(
+
+        <nav class="bg-purple-700 text-white text-2xl rounded-md p-4">
+            <div class="container mx-auto">
+                <h3 class="text-red-100">Pronóstico del tiempo</h3>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
