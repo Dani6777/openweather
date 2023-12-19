65325ccdd2406f1e9e99ad8f31d662d29864f35f
Daniel Morales <daniel.morales23@outlool.cl>
2023-12-19 05:01:35 -0300

    ADD card

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,6 +10,13 @@ const Card = ({ loadingData, showData, weather, forecast }) => {
 
     var url = '';
     var iconUrl = '';
+    var iconUrl3 = '';
+    var iconUrl6 = '';
+    var iconUrl9 = '';
+
+    var forecastDate3 = '';
+    var forecastDate6 = '';
+    var forecastDate9 = '';
 
     if (loadingData) {
         return <Spinner />;
@@ -17,14 +24,11 @@ const Card = ({ loadingData, showData, weather, forecast }) => {
 
     if (showData) {
         url = 'http://openweathermap.org/img/w/';
-
-        // Verificar si existe la propiedad weather y si tiene al menos un elemento en el array
-        if (weather.weather && weather.weather.length > 0) {
-            iconUrl = url + weather.weather[0].icon + '.png';
-        } else {
-            // Manejar el caso en que no haya datos de clima válidos
-            console.error('No hay datos de clima disponibles.');
-        }
+        iconUrl = url + weather.weather[0].icon + '.png';
+        iconUrl3 = url + forecast.list[1].weather[0].icon + '.png';
+        iconUrl6 = url + forecast.list[2].weather[0].icon + '.png';
+        forecastDate3 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' + forecast.list[1].dt_txt.substring(11, 13);
+        forecastDate6 = forecast.list[2].dt_txt.substring(8, 10) + '/' + forecast.list[2].dt_txt.substring(5, 7) + '/' + forecast.list[2].dt_txt.substring(0, 4) + ' ' + forecast.list[2].dt_txt.substring(11, 13);
     }
 
     return (
@@ -36,7 +40,7 @@ const Card = ({ loadingData, showData, weather, forecast }) => {
                             <div className="md:flex-shrink-0 transform translate-y-[-15%] md:translate-y-0 md:scale-105 relative">
                                 {/* Información del clima */}
                                 <div className="absolute top-0 left-0 right-0 text-center bg-violet-500 text-black p-2 opacity-75 z-20">
-                                    <h3 className="mb-2">{weather.name}</h3>
+                                    <h3 className="mb-2 mt-6">{weather.name}</h3>
                                     <p className="mb-2">{date}</p>
                                     <div className="flex items-center justify-center mb-2">
                                         <h1 className="text-4xl font-bold mr-2">{(weather.main.temp - 273.15).toFixed(1)}°C</h1>
@@ -44,15 +48,53 @@ const Card = ({ loadingData, showData, weather, forecast }) => {
                                     </div>
                                     <p className="mb-2">{weather.weather[0].description}</p>
                                 </div>
-                                {/* Imagen de fondo sin opacidad */}
                                 <img
                                     src="https://source.unsplash.com/800x600/?weather"
-                                    className="img-fluid rounded-start"
+                                    className="w-full h-full  rounded-md"
                                     alt="imagen"
                                 />
                             </div>
-                            <div className="p-4">
-                                <h3 className="text-2xl font-bold mb-2">Datos del Tiempo</h3>
+                            <div className="p-4 card-body mt-3 text-start">
+                                <div className="p-4 card-body text-start mt-2 bg-gray-100 rounded-lg shadow-md">
+                                    <h3 className="text-2xl font-bold mb-4">Datos del Tiempo</h3>
+                                    <div className="grid grid-cols-2 gap-4">
+                                        <div className="mb-4">
+                                            <h5 className="text-lg font-semibold">Temperatura máxima</h5>
+                                            <p>{(weather.main.temp_max - 273.15).toFixed(1)}°C</p>
+                                        </div>
+                                        <div className="mb-4">
+                                            <h5 className="text-lg font-semibold">Temperatura mínima</h5>
+                                            <p>{(weather.main.temp_min - 273.15).toFixed(1)}°C</p>
+                                        </div>
+                                        <div className="mb-4">
+                                            <h5 className="text-lg font-semibold">Sensación térmica</h5>
+                                            <p>{(weather.main.feels_like - 273.15).toFixed(1)}°C</p>
+                                        </div>
+                                        <div className="mb-4">
+                                            <h5 className="text-lg font-semibold">Humedad</h5>
+                                            <p>{weather.main.humidity}%</p>
+                                        </div>
+                                        <div className="mb-4">
+                                            <h5 className="text-lg font-semibold">Velocidad del viento</h5>
+                                            <p>{weather.wind.speed} m/s</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="my-5" />
+                                <div className="flex space-x-4 pl-4 mb-4">
+                                    { /* Mini Carta 1 */}
+                                    <div className="flex-shrink-0 bg-gray-200 p-4 rounded-md">
+                                        <p className="text-lg font-bold">{forecastDate3}Hrs</p>
+                                        <p className="flex items-center"><img src={iconUrl3} alt="icon" />{forecast.list[1].weather[0].description}</p>
+                                        <p className="text-xl">{(forecast.list[1].main.temp - 273.15).toFixed(1)}°C</p>
+                                    </div>
+                                    { /* Mini Carta 2 */}
+                                    <div className="flex-shrink-0 bg-gray-200 p-4 rounded-md">
+                                        <p className="text-lg font-bold">{forecastDate6}h</p>
+                                        <p className="flex items-center"><img className="mr-2" src={iconUrl6} alt="icon" />{forecast.list[1].weather[0].description}</p>
+                                        <p className="text-xl">{(forecast.list[1].main.temp - 273.15).toFixed(1)}°C</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
